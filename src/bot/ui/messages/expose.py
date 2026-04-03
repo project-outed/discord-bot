8ba92e650244e4cebf1d2a4051c58569a3ba0e1b
Ghost_the_killer <lucashmarcussen@gmail.com>
2026-04-03 22:36:03 +0200

    Edited expose

--- src/bot/ui/messages/expose.py
+++ src/bot/ui/messages/expose.py
@@ -1,4 +1,3 @@
-import os 
 import discord
 from discord import ui
 
@@ -11,7 +10,7 @@ class ExposeView(discord.ui.LayoutView):
 
         container.add_item(
             ui.TextDisplay(
-                "### **OFFICIAL DISCIPLINARY NOTIFICATION**\n\n"
+                "### **RULE VIOLATION NOTICE**\n\n"
                 "**TARGET IDENTIFICATION**\n"
                 f"Name: `{self.data['target_username']}`\n"
                 f"Platform ID: `{self.data['target_user_id']}`\n\n"
@@ -19,8 +18,6 @@ class ExposeView(discord.ui.LayoutView):
                 f"Software Detected: `{self.data['cheat']}`\n"
                 f"Game Environment: `{self.data['game']}`\n"
                 f"Calculated Trust Score: `{self.data['trust_score']}/100`\n\n"
-                "**LEGAL DISCLOSURE**\n"
-                "Under the GDPR and applicable data protection laws we do not publish or display underlying evidence files or media here. This is in accordance with Danish data protection regulations regarding the processing of personal data. See our Privacy Policy for how we handle personal data."
             )
         )
 
